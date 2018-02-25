69cab70fd78ca970df64cee94f97dc3bbef975fc
Ar-Sibi <getgoingwithsibi@gmail.com>
2018-02-25 17:12:41 +0530

    Initialize with webpack,semantic ui

---
+++ components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="ui card">
+            	Tis Ignitte
+	    </div>
+        );
+    }
+}
+export default App;
